b8c969b7974b1dd8250c9e07d29421c45645c1ec
Saurabh Lingam <141753821+SaurabhLingam@users.noreply.github.com>
2026-07-17 16:29:30 +0530

    Bulk sync: 1 submission

---
+++ Data Structures & Algorithms/permutation-string/submission-15.py
@@ -0,0 +1,23 @@
+class Solution:
+    def checkInclusion(self, s1: str, s2: str) -> bool:
+        left = 0
+        right = 0
+        freq = {}
+        for ch in s1:
+            freq[ch] = freq.get(ch, 0) + 1
+        need = len(freq)
+        while right < len(s2):
+            if s2[right] in freq:
+                freq[s2[right]] -= 1
+                if freq[s2[right]] == 0:
+                    need -= 1
+            if right - left + 1 > len(s1):
+                if s2[left] in freq:
+                    if freq[s2[left]] == 0:
+                        need += 1
+                    freq[s2[left]] += 1
+                left += 1
+            if need == 0:
+                return True
+            right += 1
+        return False
