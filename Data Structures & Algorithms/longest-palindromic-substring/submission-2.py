e32c1644c55bdec0f09ed7af89229e829a00d6fb
Saurabh Lingam <141753821+SaurabhLingam@users.noreply.github.com>
2026-07-31 12:09:00 +0530

    Add: longest-palindromic-substring - submission-2

---
+++ Data Structures & Algorithms/longest-palindromic-substring/submission-2.py
@@ -0,0 +1,16 @@
+class Solution:
+    def helper(self, left, right, s: str):
+        while left >= 0 and right < len(s) and s[left] == s[right]:
+            left -= 1
+            right += 1
+        return s[left + 1 : right]
+    def longestPalindrome(self, s: str) -> str:
+        curr = ""
+        for i in range(len(s)):
+            odd = self.helper(i, i, s)
+            even = self.helper(i, i+1, s)
+            if len(curr) < len(odd):
+                curr = odd
+            if len(curr) < len(even):
+                curr = even  
+        return curr     
